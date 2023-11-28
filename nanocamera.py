add872d6b9baf29393ea78bbe842e7b1cdf689a1
JiHyun-Jo7 <141097551+JiHyun-Jo7@users.noreply.github.com>
2023-11-28 13:19:40 +0900

    Using Camera

---
+++ nanocamera.py
@@ -0,0 +1,19 @@
+import cv2
+import nanocamera as nano
+
+if __name__ == '__main__':
+    # camera = nano.Camera(flip=2, width=640, height = 480, fps=30)
+    camera = nano.Camera(camera_type=1, device_id=1, width=640, height=480, fps=30)
+    # print("CSI Camera ready?:", camera.isReady())
+    print("USB Camera ready?:", camera.isReady())
+
+while camera.isReady():
+    try:
+        frame = camera.read()
+        cv2.imshow("Video Frame", frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    except KeyboardInterrupt:
+        break
+camera.release()
+del camera
